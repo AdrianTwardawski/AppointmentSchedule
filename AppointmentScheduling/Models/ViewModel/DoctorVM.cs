c6d59dbaa85097b956f6695cd3c9d875f01e1afc
adriantwardy <adriantwardawski@gmail.com>
2021-03-17 19:49:35 +0100

    Appointment Setup

---
+++ AppointmentScheduling/Models/ViewModel/DoctorVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Models.ViewModel
+{
+    public class DoctorVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
