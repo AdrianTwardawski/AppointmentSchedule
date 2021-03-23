a58e85d0640cc46901b330b272afce9557a1fd3c
adriantwardy <adriantwardawski@gmail.com>
2021-03-23 19:11:32 +0100

    Display Patient Name and Status

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -82,6 +82,15 @@ function onShowModal(obj, isEventDetail) {
         $("#patientId").val(obj.patientId);
         $("#id").val(obj.id);
 
+        $("#lblPatientName").html(obj.patientName);
+        $("#lblDoctorName").html(obj.doctorName);
+        if (obj.isDoctorApproved) {
+            $("#lblStatus").html('Approved');
+        }
+        else {
+            $("#lblStatus").html('Pending');
+        }
+
     }
     else {
         $("#appointmentDate").val(obj.startStr + " " + new moment().format("hh:mm A"));
